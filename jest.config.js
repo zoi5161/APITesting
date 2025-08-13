7fea694d014cc1dc882f84718c29a4d18a2b1bec
Trần Bảo <tranbao@MacBook-Pro-cua-Tran.local>
2025-08-13 21:58:00 +0700

    Add files

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testEnvironment: 'node',          // Chạy tests trong môi trường Node.js
-  verbose: true,                   // Hiển thị chi tiết kết quả test
+  testEnvironment: 'node',
+  verbose: true,
   testMatch: [
-    "**/tests/**/*.test.js"        // Tìm kiếm các file test trong thư mục tests
+    "**/test-scripts/**/*.test.js",  // Đảm bảo Jest tìm kiếm trong thư mục chứa test scripts
   ],
-  collectCoverage: true,           // Thu thập thông tin coverage
-  coverageDirectory: "coverage",   // Lưu trữ thông tin coverage vào thư mục 'coverage'
+  collectCoverage: true,
+  coverageDirectory: "coverage",
 };
